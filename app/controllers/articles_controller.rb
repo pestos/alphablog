a5330dcc7371d3bb8fea69bf439334e58f2e4f96
pestos <gueirard.frederic@gmail.com>
2018-06-11 18:46:59 +0000

    reussit a save un article a partir du site

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+   
+   def new
+       @articles = Article.new
+
+    end
+    
+    def create
+        #render plain: params[:article].inspect
+        @article = Article.new(article_params) 
+        @article.save
+        redirect to article_show(@article)
+    
+    end
+   
+   private
+   
+   def article_params
+        params.require(:article).permit(:title, :description)
+    
+       
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :articles
 
+    root 'pages#home'
+    get 'pages/about', to: 'pages#about'
 
-root 'pages#home'
-get 'pages/about', to: 'pages#about'
 
 end
 
